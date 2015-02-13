a7cf546cf93b9dd775a64ddfef1f9c1c1363ff38
Bernhard Schussek <bschussek@gmail.com>
2015-02-13 19:35:15 +0100

    Updated the DefaultApplicationConfig

--- src/Config/DefaultApplicationConfig.php
+++ src/Config/DefaultApplicationConfig.php
@@ -16,6 +16,8 @@ use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\ProcessHelper;
 use Symfony\Component\Console\Helper\QuestionHelper;
+use Webmozart\Console\Api\Args\Format\Argument;
+use Webmozart\Console\Api\Args\Format\Option;
 use Webmozart\Console\Api\Config\ApplicationConfig;
 use Webmozart\Console\Api\Input\InputArgument;
 use Webmozart\Console\Api\Input\InputOption;
@@ -41,24 +43,24 @@ class DefaultApplicationConfig extends ApplicationConfig
                 new QuestionHelper(),
             )))
 
-            ->addOption('help', 'h', InputOption::VALUE_NONE, 'Display help about the command')
-            ->addOption('quiet', 'q', InputOption::VALUE_NONE, 'Do not output any message')
-            ->addOption('verbose', 'v', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug')
-            ->addOption('version', 'V', InputOption::VALUE_NONE, 'Display this application version')
-            ->addOption('ansi', null, InputOption::VALUE_NONE, 'Force ANSI output')
-            ->addOption('no-ansi', null, InputOption::VALUE_NONE, 'Disable ANSI output')
-            ->addOption('no-interaction', 'n', InputOption::VALUE_NONE, 'Do not ask any interactive question')
+            ->addOption('help', 'h', Option::VALUE_NONE, 'Display help about the command')
+            ->addOption('quiet', 'q', Option::VALUE_NONE, 'Do not output any message')
+            ->addOption('verbose', 'v', Option::VALUE_OPTIONAL, 'Increase the verbosity of messages: "-v" for normal output, "-vv" for more verbose output and "-vvv" for debug', null, 'level')
+            ->addOption('version', 'V', Option::VALUE_NONE, 'Display this application version')
+            ->addOption('ansi', null, Option::VALUE_NONE, 'Force ANSI output')
+            ->addOption('no-ansi', null, Option::VALUE_NONE, 'Disable ANSI output')
+            ->addOption('no-interaction', 'n', Option::VALUE_NONE, 'Do not ask any interactive question')
 
             ->beginCommand('help')
                 ->setDescription('Display the manual of a command')
-                ->addArgument('command', InputArgument::OPTIONAL, 'The command name')
-                ->addArgument('sub-command', InputArgument::OPTIONAL, 'The sub command name')
-                ->addOption('all', 'a', InputOption::VALUE_NONE, 'Print all available commands')
-                ->addOption('man', 'm', InputOption::VALUE_NONE, 'Output the help as man page')
-                ->addOption('ascii-doc', null, InputOption::VALUE_NONE, 'Output the help as AsciiDoc document')
-                ->addOption('text', 't', InputOption::VALUE_NONE, 'Output the help as plain text')
-                ->addOption('xml', 'x', InputOption::VALUE_NONE, 'Output the help as XML')
-                ->addOption('json', 'j', InputOption::VALUE_NONE, 'Output the help as JSON')
+                ->addArgument('command', Argument::OPTIONAL, 'The command name')
+                ->addArgument('sub-command', Argument::OPTIONAL, 'The sub command name')
+                ->addOption('all', 'a', Option::VALUE_NONE, 'Print all available commands')
+                ->addOption('man', 'm', Option::VALUE_NONE, 'Output the help as man page')
+                ->addOption('ascii-doc', null, Option::VALUE_NONE, 'Output the help as AsciiDoc document')
+                ->addOption('text', 't', Option::VALUE_NONE, 'Output the help as plain text')
+                ->addOption('xml', 'x', Option::VALUE_NONE, 'Output the help as XML')
+                ->addOption('json', 'j', Option::VALUE_NONE, 'Output the help as JSON')
             ->end()
         ;
     }
